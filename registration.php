6ac7f1b158106adaad9938a2aa7b8578729cc97e
Denis Aleksandrov <denis.v.aleksandrov@gmail.com>
2026-03-18 22:13:21 +0100

    Added registration.php, etc/module.xml and composer.json

---
+++ registration.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Denal05_JunoTheme',
+    __DIR__
+);
